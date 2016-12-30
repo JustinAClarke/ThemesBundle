d58b3c363177873c7e5675e214c5e9efb560bddf
Justin Fuhrmeister-Clarke <justy531@gmail.com>
2016-12-31 08:52:50 +1100

    Change Background file upload working, and change back to default.
    Changing Themes also working.
    
    Just have to create README and composer files.
    
    may also create a web based file editor for the theme files

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -21,7 +21,7 @@ class DefaultController extends Controller
         return $this->render('ThemesBundle:Default:footer.html.twig',array('css'=>$cssTheme,'js'=>$jsTheme) );
     }
 
-    public function changebgAction()
+    public function changethemeAction()
     {
 	chdir('../src/ThemesBundle/');
 	include 'Controller/currentTheme.php';
@@ -42,6 +42,66 @@ class DefaultController extends Controller
 
 	$availThemes=$files;
 	//$currentTheme=getcwd();
-        return $this->render('ThemesBundle:Default:changebg.html.twig',array('css'=>$cssTheme,'js'=>$jsTheme,'availThemes'=>$availThemes,'currTheme'=>$currentTheme) );
+        return $this->render('ThemesBundle:Default:changeTheme.html.twig',array('css'=>$cssTheme,'js'=>$jsTheme,'availThemes'=>$availThemes,'currTheme'=>$currentTheme) );
+    }
+    public function changebgAction()
+    {
+	$errors=false;
+	$success=false;
+	$debug=false;
+	$target_file = "./bundles/themes/background.jpg";
+	$overtarget_file = "../src/ThemesBundle/Resources/public/background.jpg";
+	$defaultovertarget_file = "../src/ThemesBundle/Resources/public/defaultbg.jpg";
+
+	if(isset($_POST["submit"])) {
+	$debug=print_r($_FILES,true);
+	$target_dir = "uploads/";
+	$target_file = $target_dir . basename($_FILES["newbg"]["name"]);
+	$uploadOk = 1;
+	$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+	// Check if image file is a actual image or fake image
+	if(isset($_POST["submit"])) {
+	    $check = getimagesize($_FILES["newbg"]["tmp_name"]);
+	    if($check !== false) {
+	        $debug .="File is an image - " . $check["mime"] . ".\n";
+	        $uploadOk = 1;
+	    } else {
+	        $errors.= "File is not an image.<br>";
+	        $uploadOk = 0;
+	    }
+	}
+	// Check if file already exists
+	if (file_exists($target_file)) {
+	    unlink($target_file);
+//	    echo "Sorry, file already exists.";
+//	    $uploadOk = 0;
+	}
+	// Allow certain file formats
+	if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+	&& $imageFileType != "gif" ) {
+	    $errors.= "Sorry, only JPG, JPEG, PNG & GIF files are allowed.<br>";
+	    $uploadOk = 0;
+	}
+	// Check if $uploadOk is set to 0 by an error
+	if ($uploadOk == 0) {
+	    $errors.= "Sorry, your file was not uploaded.<br>";
+	// if everything is ok, try to upload file
+	} else {
+	    if (move_uploaded_file($_FILES["newbg"]["tmp_name"], $target_file)) {
+		copy($target_file,$overtarget_file);
+	        $success.= "The file ". basename( $_FILES["newbg"]["name"]). " has been uploaded.<br>";
+	    } else {
+	        $errors.= "Sorry, there was an error uploading your file.<br>";
+	    }
+	}
+	}
+	if(isset($_POST["default"])) {
+		if(copy($defaultovertarget_file,$overtarget_file))
+			$success.="Copyied default to /src/<br>";
+		if(copy($defaultovertarget_file,$target_file))
+			$success.="Copyied default to /web/bundles<br>";
+
+	}
+        return $this->render('ThemesBundle:Default:changebg.html.twig', array('error'=>$errors,'success'=>$success,'debug'=>$debug ) );
     }
 }
